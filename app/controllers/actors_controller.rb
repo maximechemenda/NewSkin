978ad8b466d33a169d095eb331469b101db65098
Maxime Chemenda <maxime.chemenda@gmail.com>
2020-07-09 15:37:54 +0200

    done with crud operations

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -8,7 +8,40 @@ class ActorsController < ApplicationController
     @actor = Actor.find params[:id]
   end
 
+  def new
+    @actor = Actor.new
+  end
+
+  def create
+    actor = Actor.create(actor_params)
+
+    redirect_to actors_path
+  end
+
+  def destroy
+    @actor = Actor.find(params[:id])
+    print @actor
+    @actor.destroy
+
+    redirect_to actors_path
+  end
+
+  def edit
+    @actor = Actor.find(params[:id])
+  end
+
+  def update
+    @actor = Actor.find(params[:id])
+    @actor.update(actor_params)
+
+    redirect_to actor_path(@actor)
+  end
+
+  private #Pourquoi on met les actors_params en private ? 
+  
+  def actor_params
+    params.require(:actor).permit(:name, :description)
+  end
+  
+
 end
-
-
-
--- test/controllers/actors_controller_test.rb
+++ test/controllers/actors_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ActorsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get new" do
+    get actors_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get actors_edit_url
+    assert_response :success
+  end
+
 end
